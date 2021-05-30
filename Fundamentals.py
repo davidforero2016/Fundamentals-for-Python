e952ac1a70a721b2dce3a41bb7ee2dcc12ce139d
davidforero2016@gmail.com <davidforero2016@gmail.com>
2021-05-30 16:14:51 -0500

    Firt upload for Python Fundamentals

---
+++ Fundamentals.py
@@ -0,0 +1,32 @@
+#This proyect contains the very first fundamental notions for python using.
+#To present the result of a program in screen, is used the function print(). For instance: 
+print(5)
+
+#DATA TYPES
+#Mainly, Python works with this classes os data: string, int, float, boolean list, tuple, None, dictionary.
+#The function type() hallow to know the class of a data.
+#string
+print("Charles Babbage")
+#Int
+print(17)
+#Float
+print(1.5)
+#Boolean
+print(True, False)
+#List (a list is dynamic)
+print([4,5,"Charles Babbage",1.5,True,None])
+#Tuple (a tuple is not dynamic)
+print((4,5,"Charles Babbage",1.5,True,None))
+print(None)
+#Dictionary (consist of a key-value pair)
+print({"Name":"Charles","Las taname":"Babbage","Age":17,"Hight":1.8,"Married":True})
+
+#VARIABLES
+#Write de variables's name=the value
+#Capital Letters are reserved to constans
+name="Charles"
+Last_name="Babbage"
+Age,Civil_status=30,"single"
+print(name,Last_name,Age,Civil_status)
+PI=3.1416
+print(PI)
