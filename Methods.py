82993708ab0b6b1e0297745e97309db73d2a6ef0
davidforero2016@gmail.com <davidforero2016@gmail.com>
2021-07-22 17:11:27 -0500

    Buffer 2207211711

---
+++ Methods.py
@@ -0,0 +1,17 @@
+#methods are subroutines that allow us tho have information about variables, or make changes on it. Fir instance, type()
+#Here are presented the main methods for strings.
+Example_Variable=("John Benet")
+
+print(dir(Example_Variable))
+print(Example_Variable.upper())
+print(Example_Variable.lower())
+print(Example_Variable.swapcase())
+print(Example_Variable.capitalize())
+print(Example_Variable.replace("oh","ua"))
+print(Example_Variable)
+print(Example_Variable.count("h"))
+print(Example_Variable.startswith("J"))
+print(Example_Variable.endswith("m"))
+print(Example_Variable.split())
+print(Example_Variable.find("B"))
+print(len(Example_Variable))
