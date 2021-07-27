28ba57184a398a04b3c9f6a83638ea38e45eb2f2
davidforero2016@gmail.com <davidforero2016@gmail.com>
2021-07-27 12:42:32 -0500

    2707211242

---
+++ Numbers.py
@@ -0,0 +1,15 @@
+#This file contains the fundamental operations with numbers.
+
+print(2+3)
+print(2-3)
+print(2*3)
+print(2**3)
+print(2/3)
+print(3/2)
+print(3//2)
+print(3%2)
+
+#The function input:
+age=input("Insert your age: ")
+new_age=int(age)+5
+print(new_age)
