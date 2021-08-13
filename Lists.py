072f82cec44db038f695b9b09a51da7c882e4f3b
davidforero2016@gmail.com <davidforero2016@gmail.com>
2021-08-13 10:57:10 -0500

    1308211057

---
+++ Lists.py
@@ -0,0 +1,8 @@
+#This file contains fundamental notions about lists.
+Demolist1=[1, "John", True, 1,8, [1,2,3,4]]
+print(Demolist1)
+Demolist2=list((1, "John", True, 1,8, [1,2,3,4]))
+print(Demolist2)
+Demolist3=list(range(0,10))
+print(Demolist3)
+print(len(Demolist1))
