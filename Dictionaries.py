0c4a5fe04dd9eced8325c419807caa81fea98cb9
davidforero2016@gmail.com <davidforero2016@gmail.com>
2021-08-13 11:50:01 -0500

    1308211149

---
+++ Dictionaries.py
@@ -0,0 +1,6 @@
+#Dictionaries have a key and a value.
+Costumerdata={
+(30,"single","Male"):"Carlos",
+(27,"married","Female"):"Juana"
+}
+print(Costumerdata)
---
+++ Tuples.py
@@ -0,0 +1,7 @@
+x=(1,)
+y=(1,2,3)
+z=tuple((2,4,6))
+print(x,y,z)
+print(z[2])
+#del is used to delete the tuple
+print(dir(z))
