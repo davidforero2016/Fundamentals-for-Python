e35121aebd7bca417bdc09c0073bb172f31db3bb
davidforero2016@gmail.com <davidforero2016@gmail.com>
2021-08-19 11:56:43 -0500

    1908211156

--- Sets.py
+++ Sets.py
@@ -3,3 +3,10 @@ Colors={"Yellow","Blue","Red"}
 print(type(Colors))
 print(dir(Colors))
 print("Yellow" in Colors)
+Colors.remove("Blue")
+print(Colors)
+Colors.clear()
+print(Colors)
+del Colors
+print(Colors)
+
